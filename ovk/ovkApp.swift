d1604547eaf1ce0cf81976aebcad28168c21fd6a
IsamiRi <dimawolfg@gmail.com>
2023-10-23 17:51:20 +0300

    Initial Commit

---
+++ ovk/ovkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ovkApp.swift
+//  ovk
+//
+//  Created by Isami Riša on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ovkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
